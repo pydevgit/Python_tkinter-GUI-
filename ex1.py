1a3e8940ad3c8c7ff0f15e436d84c698351f8f1b
Nitin <pydev5676@gmail.com>
2020-01-29 19:20:16 +0530

    new commit in this file

--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
+# import all class inside of tkinter library
 from tkinter import *
 
 root = Tk()
-myLabel = Label(root, text = "I,m root window ")
+# here define a label nothing but (text)
+myLabel = Label(root, text="I,m root window ")
 myLabel.pack
-
+# it hold the window
 root.mainloop()
